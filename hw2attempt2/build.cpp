c8f13539cc9a1a539976b176cec8b97032220fdf
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2020-10-06 12:20:54 -0800

    passing no bridges

---
+++ hw2attempt2/build.cpp
@@ -0,0 +1,10 @@
+#include "build.h"
+//build.cpp
+//Ben Talotta
+//10/6/2020
+//for use in cs411 hw 2
+
+int build(int w, int e, const vector<Bridge>& bridges)
+{
+	return 0;
+}
---
+++ hw2attempt2/build.h
@@ -0,0 +1,21 @@
+//build.h
+//Ben Talotta
+//9/28/2020
+//for use in cs411 hw 2
+
+#ifndef build_HPP
+#define build_HPP
+#include <vector>
+#include <algorithm>
+using std::vector;
+#include<iostream>//for testing delete later
+using std::cout;//for testing delete later 
+using Bridge = vector<int>;
+int build(int w, int e, const vector<Bridge>& bridges);
+
+
+
+
+
+
+#endif "!build_H"
