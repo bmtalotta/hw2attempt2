b8b6d22e5209279f07baa1bfda335d49a174c1b4
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2020-10-07 12:10:04 -0800

    more tests passing

--- hw2attempt2/build.cpp
+++ hw2attempt2/build.cpp
@@ -8,37 +8,51 @@ int build(int w, int e, const vector<Bridge>& bridges)
 {
 	int n = bridges.size();
 	int currentSum = 0;
+	int posProf = 0;
 	vector<Bridge> tempSub;
 	vector<vector<Bridge>> allGoodSubsets;
-	vector<int> profit = {0};
-	for (int i = 0; i < ((n * n) - 1); ++i) {
+	vector<vector<Bridge>> allSubsets;
+	vector<int> profit = { 0 };
+	for (int i = 0; i < n*n; i++) {
+		int temp = i;
+		posProf = 0;
 		tempSub.clear();
-		for (int j = 0; j < n - 1; ++j) {
-			if (i & (1 << j)) {
+		for (int j = 0; j < n; j++) {
+			if ((temp & 1)) {
+				posProf += bridges.at(j).at(2);
 				tempSub.push_back(bridges.at(j));
 			}
+			temp >>= 1;
 		}
-		if (tempSub.size() > 0) {
-			if (checkSubset(tempSub)) {
-				allGoodSubsets.push_back(tempSub);
-			}
-		}	
+		allSubsets.push_back(tempSub);
+		if (checkSubset(tempSub)) {
+			allGoodSubsets.push_back(tempSub);
+			profit.push_back(posProf);
+		}
+			
 	}
+	for (auto n : profit) {
+		cout << n << ", ";
+	}
+	cout << "\n";
+	//cout << *max_element(profit.begin(), profit.end()) << "\n";
+/*
 	if (allGoodSubsets.size() > 0) {
-		for (int sub = 0; sub < allGoodSubsets.size() - 1; ++sub) {
-			for (int k = 0; k < allGoodSubsets.at(sub).size() - 1; ++k) {
+		for (int sub = 0; sub < allGoodSubsets.size(); sub++) {
+			for (int k = 0; k < allGoodSubsets.at(sub).size(); k++) {
 				currentSum += allGoodSubsets.at(sub).at(k).at(2);
 			}
 			profit.push_back(currentSum);
 			currentSum = 0;
 		}
 	}
+	*/
 	return *max_element(profit.begin(),profit.end());
 }
 bool checkSubset(vector<Bridge> subset)
 {
-	for (int i = 0; i < (subset.size() - 1); ++i) {
-		for (int j = 0; j < (subset.size() - 1); ++j) {
+	for (int i = 0; i < subset.size(); i++) {
+		for (int j = 0; j < subset.size(); j++) {
 			if (j > i) {
 				if (((subset.at(i).at(0) == subset.at(j).at(0)) || (subset.at(i).at(1) == subset.at(j).at(1))) ||  //two bridges same city
 					((subset.at(i).at(0) > subset.at(j).at(0)) && (subset.at(i).at(1) < subset.at(j).at(1))) ||	//first way bridges can cross
